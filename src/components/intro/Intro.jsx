d046858d330a106e7d82b3e48cdc24b7b852e3dd
Mor Khojasteh <mori.kh1987@gmail.com>
2022-03-28 22:53:37 +0100

    feat: add style for the intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./intro.css";
 
 const Intro = () => {
   return (
-    <div>this is intro</div>
-  )
-}
+    <div className="intro">
+      <div className="intro-left">
+        <div className="intro-left-wrapper">
+          <h2 className="intro-body">Hello, My name is</h2>
+          <h2 className="intro-name">Morteza Khojasteh</h2>
+          <div className="intro-title">
+            <div className="intro-title-wrapper">
+              <div className="intro-title-item">Web Developer</div>
+              <div className="intro-title-item">UI/UX Designer</div>
+              <div className="intro-title-item">Photographer</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="intro-right">Right</div>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
