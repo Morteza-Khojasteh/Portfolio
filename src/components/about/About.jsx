4d0fe478d14232267c9a90fedbf6a33bd73ffb92
Mor Khojasteh <mori.kh1987@gmail.com>
2022-03-30 21:55:40 +0100

    feat: add about component and css

---
+++ src/components/about/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./about.css";
+
+export const About = () => {
+  return (
+    <div className="about">
+      <div className="about-left">
+        <div className="about-card"></div>
+        <div className="about-card"></div>
+      </div>
+      <div className="about-right"></div>
+    </div>
+  );
+};
+
+export default About;
