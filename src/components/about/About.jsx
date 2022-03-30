0424664a436ca7b510ead9c8dafdb938bd0dd7ec
Mor Khojasteh <mori.kh1987@gmail.com>
2022-03-30 22:18:15 +0100

    feat: add description for about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./about.css";
+import Award from "../../img/award.png"
 
 export const About = () => {
   return (
@@ -14,7 +15,31 @@ export const About = () => {
           />
         </div>
       </div>
-      <div className="about-right"></div>
+      <div className="about-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="about-sub">
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className="about-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+        <div className="about-award">
+          <img src={Award} alt="" className="about-award-img" />
+          <div className="about-award-texts">
+            <h4 className="about-award-title">
+              International Design Awards 2021
+            </h4>
+            <p className="about-award-desc">
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
+              and fugit.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
