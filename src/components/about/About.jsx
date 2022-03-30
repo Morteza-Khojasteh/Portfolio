5b3e0e4bf3fb1d2b93b30ac5aa829a3d60ef9214
Mor Khojasteh <mori.kh1987@gmail.com>
2022-03-30 22:07:27 +0100

    feat: add img and style for about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,8 +5,14 @@ export const About = () => {
   return (
     <div className="about">
       <div className="about-left">
-        <div className="about-card"></div>
-        <div className="about-card"></div>
+        <div className="about-card bg"></div>
+        <div className="about-card">
+          <img
+            src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="about-img"
+          />
+        </div>
       </div>
       <div className="about-right"></div>
     </div>
