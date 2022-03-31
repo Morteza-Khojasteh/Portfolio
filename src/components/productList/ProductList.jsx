548933be078f8aa6e996b3cfc929aa7e89fc2a17
Mor Khojasteh <mori.kh1987@gmail.com>
2022-03-31 23:12:46 +0100

    feat: add content and style to product list

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-import "./productList.css"
+import "./productList.css";
+import Product from "../product/Product";
+
 export const ProductList = () => {
-  return <div>ProductList</div>;
+  return <div className="product-list">
+      <div className="product-list-text">
+      <h1 className="product-list-title">Create & inspire. It's Lama</h1>
+        <p className="product-list-desc">
+          Lama is a creative portfolio that your work has been waiting for.
+          Beautiful homes, stunning portfolio styles & a whole lot more awaits
+          inside.
+        </p>
+      </div>
+      <div className="product-lists">
+          <Product />
+        {/* {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))} */}
+      </div>
+      </div>;
 };
 
 
