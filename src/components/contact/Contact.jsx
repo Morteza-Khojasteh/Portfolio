9f219cb66618138781336d1a6a030f92ec0648be
Mor Khojasteh <mori.kh1987@gmail.com>
2022-04-01 19:02:25 +0100

    feat: add contact component and style

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-bg"></div>
+      <div className="contact-wrapper">
+          <div className="contact-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          
+          </div>
+          <div className="contact-right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
