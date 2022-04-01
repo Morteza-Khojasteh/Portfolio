280e1b09d86b3bf3c0f867dc802c1e790b43fd86
Mor Khojasteh <mori.kh1987@gmail.com>
2022-04-01 19:21:00 +0100

    feat: add contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
 
 const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-bg"></div>
       <div className="contact-wrapper">
-          <div className="contact-left">
-          <h1 className="c-title">Let's discuss your project</h1>
-          
+        <div className="contact-left">
+          <h1 className="contact-title">Let's discuss your project</h1>
+          <div className="contact-info">
+            <div className="contact-info-item">
+              <img src={Phone} alt="" className="contact-icon" />
+              +4473000 00 00
+            </div>
+            <div className="contact-info-item">
+              <img src={Email} alt="" className="contact-icon" />
+              morteza.khojaste@gmail.com
+            </div>
+            <div className="contact-info-item">
+              <img src={Address} alt="" className="contact-icon" />
+              UK, Manchester
+            </div>
           </div>
-          <div className="contact-right"></div>
+        </div>
+        <div className="contact-right">
+          <p className="contact-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Message" name="user_message" />
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
