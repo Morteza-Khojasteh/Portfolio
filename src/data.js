2b2d8e2294b11d33add9aea6e07547acf272bcf8
Mor Khojasteh <mori.kh1987@gmail.com>
2022-04-06 16:03:24 +0100

    feat: add screnshot of projects

--- src/data.js
+++ src/data.js
@@ -1,32 +1,17 @@
 export const products = [
     {
       id: 1,
-      img: "https://pikwizard.com/photos/close-up-of-various-presents-and-christmas-tree--3495f9a790757d476472c96953e5aba4-m.jpg",
-      link: "http://lama.dev",
+      img: "http://localhost:3000/img/Projects-img/development-planner-cyf-final.png",
+      link: "https://development-planner-cyf-final.herokuapp.com/",
     },
     {
       id: 2,
-      img: "https://pikwizard.com/photos/technician-using-digital-tablet--a57e8a0e3ae90d466241dab1bcdb4c71-m.jpg",
-      link: "http://lama.dev",
+      img: "http://localhost:3000/img/Projects-img/hotel-react.png",
+      link: "https://cyf-morteza-khojasteh-hotel-react.netlify.app/",
     },
     {
       id: 3,
-      img: "https://pikwizard.com/photos/coffee-writing-computer-blogging--832c6a214586de8781a11ec936003908-m.jpg",
-      link: "http://momo",
-    },
-    {
-      id: 4,
-      img: "https://pikwizard.com/photos/businesspeople-interacting-inside-office-building--eb7113d5053f28a1fed9821d790722a2-m.jpg",
-      link: "http://momo",
-    },
-    {
-      id: 5,
-      img: "https://pikwizard.com/photos/chairs-empty-office-room--053f82c115cdd97090a18dfc1d8674c2-m.jpg",
-      link: "http://momo",
-    },
-    {
-      id: 6,
-      img: "https://pikwizard.com/photos/chairs-empty-office-room--053f82c115cdd97090a18dfc1d8674c2-m.jpg",
-      link: "http://momo",
-    },
+      img: "http://localhost:3000/img/Projects-img/tv-show.png",
+      link: "https://cyf-morteza-khojasteh-tv.netlify.app/",
+    }
   ];
